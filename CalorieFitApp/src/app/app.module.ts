b76ebd3c23de89521a40cf1165de5def3d59b0df
OmarJ03 <omj7473@rit.edu>
2024-11-11 16:35:28 +0400

    Create initiation-page component

--- CalorieFitApp/src/app/app.module.ts
+++ CalorieFitApp/src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InitiationPageComponent } from './initiation-page/initiation-page.component';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
+    InitiationPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule
   ],
   providers: [
-    provideClientHydration(),
-    provideHttpClient(withFetch()) // Enable fetch for HttpClient
+    provideClientHydration()
   ],
   bootstrap: [AppComponent]
 })
