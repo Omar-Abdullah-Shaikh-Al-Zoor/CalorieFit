54261c869b994c38b7bcd2d0ca503e3fff4cd874
OmarJ03 <omj7473@rit.edu>
2024-11-13 17:34:33 +0400

    Added the weekly availability question to the Initiation page

--- CalorieFitApp/src/app/initiation-page/initiation-page.component.ts
+++ CalorieFitApp/src/app/initiation-page/initiation-page.component.ts
@@ -9,14 +9,15 @@ export class InitiationPageComponent {
   step: number = 1;  // Step counter to control the current question
   fitnessGoal: string = '';
   gender: string = '';
+  weeklyAvailability: string = '';  // New property for weekly availability
   
   weight: number | null = null;
-  height: number | null = null
+  height: number | null = null;
   age: number | null = null;
 
   dailyCaloricExpenditure: number = 0;
-  dailyProteinIntake: number =0;
-  dailyCarbIntake: number =0;
+  dailyProteinIntake: number = 0;
+  dailyCarbIntake: number = 0;
 
   // Function to handle selection of fitness goal
   selectGoal(goal: string): void {
@@ -32,13 +33,19 @@ export class InitiationPageComponent {
     this.nextStep();
   }
 
+  // Function to handle weekly availability selection
+  selectWeeklyAvailability(availability: string): void {
+    this.weeklyAvailability = availability;
+    console.log('Selected Weekly Availability:', this.weeklyAvailability);
+    this.nextStep();
+  }
+
   // Function to handle form submission
   submitForm(): void {
     this.onSubmit(); // Call the onSubmit method to perform calculations and update userDetails
     this.step++; // Move to the next step or show results after submitting
   }
   
-
   // Move to the next step
   nextStep(): void {
     this.step++;
@@ -54,7 +61,6 @@ export class InitiationPageComponent {
 
       this.dailyProteinIntake = this.weight; // 1 gram per kg of weight
       this.dailyCarbIntake = Math.round((this.dailyCaloricExpenditure / 2) * 0.25); // Half the daily caloric expenditure multiplied by 0.25
-
     }
   }
 }
