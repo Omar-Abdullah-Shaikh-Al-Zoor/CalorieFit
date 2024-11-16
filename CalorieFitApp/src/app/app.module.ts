e7d2378d0ff2e323b173b6e3936e4e9fd99b46ef
Omar <okm8344@tir.edu>
2024-11-16 16:49:51 +0400

    Diet page commit

--- CalorieFitApp/src/app/app.module.ts
+++ CalorieFitApp/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DietPageComponent } from './diet-page/diet-page.component';
 @NgModule({
   declarations: [
     AppComponent,
+    DietPageComponent,
   ],
   imports: [
     BrowserModule,
---
+++ CalorieFitApp/src/app/diet-page/diet-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-diet-page',
+  templateUrl: './diet-page.component.html',
+  styleUrl: './diet-page.component.css'
+})
+export class DietPageComponent {
+
+}
