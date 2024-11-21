8e293eb0c7636b8d4c43ef8a3abdb03595eac0ee
OmarJ03 <omj7473@rit.edu>
2024-11-21 20:59:49 +0400

    Created the Interface and Service for the workout-page component

---
+++ CalorieFitApp/src/app/workout.ts
@@ -0,0 +1,8 @@
+export interface Workout {
+    id: number;             // Unique identifier
+    workout: string;        // Name of the workout
+    muscleGroup: string;    // Targeted muscle group
+    day: string;            // Day of the workout
+    instructions: string;   // Workout instructions
+  }
+  
